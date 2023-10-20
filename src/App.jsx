8bcd04c75e2b36c34e5b3a8a129c6f281a11d4a1
rizkihutama <rizkihutama05@gmail.com>
2023-10-20 21:15:53 +0700

    small update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import { useSelector } from 'react-redux';
 
 export default function App() {
-  const authState = useSelector((state) => state.auth);
-
-  // const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
   return (
     <>
-      {!authState.isAuthenticated ? (
-        <Auth />
-      ) : (
-        <>
-          <Header />
-          <Counter />
-        </>
-      )}
+      <Header />
+      {!isAuth ? <Auth /> : <UserProfile />}
+      <Counter />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { logout } from '../store/slice/authSlice';
 import classes from './Header.module.css';
 
 export default function Header() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   const dispatch = useDispatch();
 
   const onLogoutHandler = () => {
@@ -12,19 +15,21 @@ export default function Header() {
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='#'>My Products</a>
-          </li>
-          <li>
-            <a href='#'>My Sales</a>
-          </li>
-          <li>
-            <button onClick={onLogoutHandler}>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href='#'>My Products</a>
+            </li>
+            <li>
+              <a href='#'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={onLogoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
